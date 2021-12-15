c65769360b53faba939ab4bc649289c5ee23e417
f11m4at4 <f11m4at4@gmail.com>
2021-12-15 14:38:10 +0900

    ray를 이용한총쏘기

---
+++ Assets/Scripts/PlayerFire.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 사용자가 발사버튼을 누르면 총을 발사하고 싶다.
+// 필요속성 : 총구(카메라)
+public class PlayerFire : MonoBehaviour
+{
+    // 필요속성 : 파편이펙트
+    public GameObject bulletEffectFactory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 사용자가 발사버튼을 누르면 총을 발사하고 싶다.
+        // 1. 사용자가 발사버튼을 눌렀으니까
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // 2. 총을 발사하고 싶다.
+            // 1) Ray 가 필요하다.
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+            RaycastHit hitInfo;
+            // 2) Ray를 던져야 한다.
+            bool result = Physics.Raycast(ray, out hitInfo);
+            // 3) Ray 가 닿은 지점에 파편을 튀게 하고 싶다.
+            if(result)
+            {
+                // 파편을 공장에서 만든다.
+                GameObject bulletEffect = Instantiate(bulletEffectFactory);
+                // 파편을 위치시킨다.
+                bulletEffect.transform.position = hitInfo.point;
+
+            }
+        }
+    }
+}
